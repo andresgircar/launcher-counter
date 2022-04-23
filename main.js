eb2b81d98b2ad0ab4be1061b172ce7267aafb508
Andrés Giraldo <andresgircar@gmail.com>
2022-04-23 13:59:54 -0500

    script terminado

--- main.js
+++ main.js
@@ -7,21 +7,20 @@ function tick () {
     let t = launchDate - now;
     if (t > 0) {
         let days = Math.floor(t / (1000 * 60 * 60 * 24));
-        if (days < 10) { days = "0" + days};
+        if (days < 10) { days = "0" + days;}
         let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        if (hours < 10) { hours = "0" + hours};
+        if (hours < 10) { hours = "0" + hours;}
         let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-        if (minutes < 10) { minutes = "0" + minutes};
+        if (minutes < 10) { minutes = "0" + minutes;}
         let seconds = Math.floor((t % (1000 * 60)) / 1000);
-        if (seconds < 10) { seconds = "0" + seconds};
-        
+        if (seconds < 10) { seconds = "0" + seconds;}
+        let showDays = `${days}`;
+        document.querySelector('.days-counter').innerText = showDays;
+        let showHours = `${hours}`;
+        document.querySelector('.hours-counter').innerText = showHours;
+        let showMinutes = `${minutes}`;
+        document.querySelector('.minutes-counter').innerText = showMinutes;
+        let showSeconds = `${seconds}`;
+        document.querySelector('.seconds-counter').innerText = showSeconds;
     }
-    let showDays = `${days}`;
-    document.querySelector('.days-counter').innerText = showDays;
-    let showHours = `${hours}`;
-    document.querySelector('.days-counter').innerText = showHours;
-    let showMinutes = `${minutes}`;
-    document.querySelector('.days-counter').innerText = showMinutes;
-    let showSeconds = `${seconds}`;
-    document.querySelector('.days-counter').innerText = showSeconds;
-}
+};
